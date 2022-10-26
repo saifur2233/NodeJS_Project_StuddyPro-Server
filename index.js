01e89380d11b41e54db9650e5909bb6dcaca6488
saifur2233 <saifurnstuiit223344@gmail.com>
2022-10-26 13:21:10 +0600

    fake data added and get data from json

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 
+const courseCategory = require('./data/course-category.json');
+
 app.use(cors());
 const port = 5000;
 
 app.get('/',(req, res)=>{
     res.send("This is home");
+})
+
+app.get('/course-category',(req, res)=>{
+    res.send(courseCategory);
 })
 
 app.listen(port,()=>{
